56555046e85df295c6866c11f03c0671f368db53
stefan9x <stefanj94@live.com>
2018-03-20 11:41:30 +0100

    Hello world added

---
+++ SDK/SDK_Workspace/hello_world/Debug/src/helloworld.d
@@ -0,0 +1,15 @@
+src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../hello_world_bsp/microblaze_0/include/xparameters.h \
+ ../../hello_world_bsp/microblaze_0/include/xio.h \
+ ../../hello_world_bsp/microblaze_0/include/xbasic_types.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../hello_world_bsp/microblaze_0/include/xparameters.h:
+
+../../hello_world_bsp/microblaze_0/include/xio.h:
+
+../../hello_world_bsp/microblaze_0/include/xbasic_types.h:
